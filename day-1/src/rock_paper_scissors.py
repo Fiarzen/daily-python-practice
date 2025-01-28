3fb3ec40c9339061ff3e74282b76950c1eb33611
Jeremy Lam <jeremylam1995@gmail.com>
2025-01-28 13:30:58 +0000

    fixed logic, added main method

--- day-1/src/rock_paper_scissors.py
+++ day-1/src/rock_paper_scissors.py
@@ -2,17 +2,22 @@
 def game(p1, p2):
     if p1 == p2:
         return ("Draw!")
-    elif p1 - p2 == -1 or p1 - p2 == 2:
+    elif p1 - p2 == 1 or p1 - p2 == -2:
         return ("Player 1 wins!")
-    elif p2 - p1 == -1 or p2 - p1 == 2:
+    elif p2 - p1 == 1 or p2 - p1 == -2:
         return ("Player 2 wins!")
     else:
         return ("Error in input")
 
 def main():
-    p1 = input("Player 1, enter 1(rock), 2(paper) or 3(scissors)")
-    p2 = input("Player 2, enter 1(rock), 2(paper) or 3(scissors)")
-    game(p1, p2)
+    p1 = int(input("Player 1, enter 1(rock), 2(paper) or 3(scissors)\n"))
+    p2 = int(input("Player 2, enter 1(rock), 2(paper) or 3(scissors)\n"))
+    print(game(p1, p2))
+    if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+        main()
+    else:
+        print('game over.')
+
 
 if __name__ == "__main__":
     main()
--- day-1/test/rock_paper_scissors_test.py
+++ day-1/test/rock_paper_scissors_test.py
@@ -5,14 +5,14 @@ def test_draw():
     assert result == "Draw!"
 
 def test_p1_win():
-    assert game(3, 1) == "Player 1 wins!"
-    assert game(1, 2) == "Player 1 wins!"
-    assert game(2, 3) == "Player 1 wins!"
-
-def test_p2_win():
     assert game(1, 3) == "Player 2 wins!"
     assert game(2, 1) == "Player 2 wins!"
     assert game(3, 2) == "Player 2 wins!"
+
+def test_p2_win():
+    assert game(3, 1) == "Player 1 wins!"
+    assert game(1, 2) == "Player 1 wins!"
+    assert game(2, 3) == "Player 1 wins!"
 
 def test_input_error():
     assert game(4, 10) == "Error in input"
